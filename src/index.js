db387ca4bffe1dab61149bc32ca61f067c45c492
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-03 04:51:15 -0500

    rendered container div and h1 for SimpleForm componenet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-test
+
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -12,12 +12,16 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  return <div className="container">
+    <h1>Simple Form App</h1>
+    
+    </div>
 }
 
 render(
   <>
     <SimpleForm />
+    
     {/* <App /> */}
   </>
   , document.querySelector('#root')
